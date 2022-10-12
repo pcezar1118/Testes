29a7e11ff12f89a4c3e247d616bcc033819a0d14
pcezar1118 <pcezar1118@gmail.com>
2022-10-12 10:13:58 -0300

    Object.create

--- script.js
+++ script.js
@@ -14,6 +14,29 @@ Carro.prototype.parar = function() {
 };
 
 
-let fusca = new Carro('Fusca', 'Azul', 'VW', 1975);
+let brasilia = new Carro('Brasília', 'Azul', 'VW', 1975);
+
+console.log(brasilia);
+
+//Como não é um construtor o nome do objeto
+//Inicia em minúsculo
+let carroProto = {
+    velocidade: 0,
+    acelerar: function() {
+        this.velocidade += 5;
+    },
+    parar: function() {
+        this.velocidade = 0;
+    }
+};
+
+//Utilizando uma função do prototype do Object
+//Para criar um objeto
+let fusca = Object.create(carroProto, {
+    nome: {value: 'Fusca'},
+    cor: {value:'Azul'},
+    marca: {value:'VW'},
+    ano: {value: 1975}
+});
 
 console.log(fusca);
